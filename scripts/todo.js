b7d00150caba6b703ad94aae682d2ae62d0586ea
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-05 16:11:03 -0400

    Add scripts and 2 html pages with nav bar linking

---
+++ scripts/todo.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("button");
+
+
+
+
+window.onload=()=>{
+    button.onclick=onButtonClicked;
+
+}
+
+
+function onButtonClicked(){
+    console.log('clicked');
+   
+}
