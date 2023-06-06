64d38253b5acca804bbdb51b4a357c82b0b42afb
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-06 10:16:43 -0400

    Add button , and api to display todo

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,15 +1,33 @@
+"use strict";
+
 const button = document.getElementById("button");
+const div = document.getElementById("messageText");
+const todoField = document.getElementById("todoField");
 
+window.onload = () => {
+  button.onclick = onButtonClicked;
 
+};
 
+function onButtonClicked() {
+    // let inputValue = 0;
 
-window.onload=()=>{
-    button.onclick=onButtonClicked;
-
+  console.log("clicked");
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((data) => {
+      for(let i=0; i<i<data.length; i++) {
+        // inputValue.valueOf = data[i].title;
+        let message1 = "User todo: " + data[i].title;
+        let message2 = "User complete " + data[i].completed;
+        let message=message1+message2;
+        div.innerHTML = message;
+        console.log(message);
+      
+      }
+    });
 }
 
+// let selectedType = todoField.value;
 
-function onButtonClicked(){
-    console.log('clicked');
-   
-}
+// const mountain = mountainsArray.find(mountain => mountain.name === mountainSelected);
