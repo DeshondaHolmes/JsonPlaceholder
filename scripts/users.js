43088a216034d8781c1ba81e29879c56977cacc5
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-14 14:11:02 -0400

    Add new page with linking

--- scripts/users.js
+++ scripts/users.js
@@ -37,19 +37,3 @@ function showUsers() {
         }
     });
 }
-//   let row = table.insertRow(-1);
-//   let cell0 = row.insertCell(0);
-//   let cell1 = row.insertCell(1);
-//   let cell2 = row.insertCell(2);
-//   let cell3 = row.insertCell(3);
-//   let cell4 = row.insertCell(4);
-//   let cell5 = row.insertCell(5);
-//   let cell6 = row.insertCell(6);
-
-//   cell0.innerHTML = data.id;
-//   cell1.innerHTML = data.name;
-//   cell2.innerHTML = data.email;
-//   cell3.innerHTML = data.phone;
-//   cell4.innerHTML = data.website;
-//   cell5.innerHTML = data.company.name;
-//   cell6.innerHTML = data.company.catchPhrase;
